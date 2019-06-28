8301050de65545581160ea8ede51bbcd3f835b2a
GlennCottam <glenn.cottam@hotmail.com>
2019-06-28 20:34:22 -0300

    Ability to switch subreddits started

--- web/js/posts.js
+++ web/js/posts.js
@@ -1,8 +1,13 @@
 // TODO: Create caching system for posts
-
 // default subreddit grab
+var reddit_url = "https://api.reddit.com/hot";
+
+// writing to "cache"
 var fs = require('fs');
-var reddit_url = "https://api.reddit.com/hot/";
+
+var last_sort = "hot";
+var last_sub = null;
+
 
 var post_count = 25;         // count
 var last_post = null;       // after2
@@ -10,13 +15,49 @@ var last_type = null;       // after1
 
 var posts;
 
-
 function imgError(image){
     image.style.display = 'none';
 }
 
+function hideElement(id)
+{
+    $(id).attr('hidden', true);
+}
+
+function showElement(id)
+{
+    $(id).attr('hidden', false);
+}
+
+
 var app = angular.module('RedDesk', []);
 app.controller('reddesk_ctrl', function($scope, $http){
+
+    $scope.getConfig = function()
+    {
+        console.log("From Settings.js: " + last_sub + last_sort);
+    }
+
+    $scope.setGetUrl = function(sub, sort)
+    {
+        console.log("Setting Subreddit");
+
+        if(sub == null)
+        {
+            reddit_url = "https://api.reddit.com/" + sort;
+            last_sort = sort;
+        }
+        else
+        {
+            reddit_url = "https://api.reddit.com/" + "r/" + sub + "/" + sort;
+            last_sort = sort;
+            last_sub = sub;
+        }
+
+        console.log("New URL" + reddit_url);
+
+        $scope.getPosts();
+    }
 
     $scope.getPosts = function()
     {
@@ -26,11 +67,25 @@ app.controller('reddesk_ctrl', function($scope, $http){
         $http.get(complete_url).then(function(response)
         {
             $scope.posts = response.data.data.children;
-            // console.log($scope.posts);
+
+
+
+            console.log($scope.posts);
             post_count = post_count + post_count;
 
             // console.log("post count = " + post_count);
-            last_post = $scope.posts[post_count - 1].data.id;
+
+            if($scope.posts[post_count - 1] == null)
+            {
+                console.log("No Data Found");
+                last_post = null;
+            }
+            else
+            {
+                last_post = $scope.posts[post_count - 1].data.id;
+            }
+
+            
             last_type = $scope.posts[post_count - 1].kind;
 
             
